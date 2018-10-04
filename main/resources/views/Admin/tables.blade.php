de606a36c9242c5c7c39cce5f0262b94ba461d93
ShivaniBalwani <shivanibalwani3@gmail.com>
2018-10-04 17:20:14 +0530

    laravel setup

---
+++ main/resources/views/Admin/tables.blade.php
@@ -0,0 +1,34 @@
+  
+      <!-- Breadcrumb-->
+      <div class="breadcrumb-holder">
+        <div class="container-fluid">
+          <ul class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item active">Tables       </li>
+          </ul>
+        </div>
+      </div>
+      <footer class="main-footer">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-6">
+              <p>Your company &copy; 2017-2019</p>
+            </div>
+            <div class="col-sm-6 text-right">
+              <p>Design by <a href="https://bootstrapious.com" class="external">Bootstrapious</a></p>
+              <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions and it helps me to run Bootstrapious. Thank you for understanding :)-->
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!-- JavaScript files-->
+    <script src="jquery/jquery.min.js"></script>
+    <script src="popper.js/umd/popper.min.js"> </script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="chart.js/Chart.min.js"></script>
+    <script src="malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
+    <!-- Main File-->
+    <script src="js/front.js"></script>
+  </body>
+</html>
