23e058dd1d3a06e6803bd9f0bfd69937ab1d1ca0
Meshwa Patel <patelmeshwa68@gmail.com>
2018-10-06 04:43:34 +0530

    updated

---
+++ Frontend/Register/Register/drive.js
@@ -0,0 +1,39 @@
+(function () {
+    "use strict";
+
+    // The initialize event handler must be run on each page to initialize Office JS.
+    // You can add optional custom initialization code that will run after OfficeJS
+    // has initialized.
+    Office.initialize = function (reason) {
+        // The reason object tells how the add-in was initialized. The values can be:
+        // inserted - the add-in was inserted to an open document.
+        // documentOpened - the add-in was already inserted in to the document and the document was opened.
+
+        // Checks for the DOM to load using the jQuery ready function.
+        $(document).ready(function () {
+            // Set your optional initialization code.
+            // You can also load saved settings from the Office object.
+        });
+    };
+
+    // Run a batch operation against the Word JavaScript API object model.
+    // Use the context argument to get access to the Word document.
+    Word.run(function (context) {
+
+        // Create a proxy object for the document.
+        var thisDocument = context.document;
+        // ...
+    })
+})();
+
+
+
+//For google translator
+
+<div id="google_translate_element"></div>
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
