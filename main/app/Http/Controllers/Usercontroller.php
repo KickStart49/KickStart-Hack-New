01e1007ad1f38e527c31307711280873cf6d3a43
ShivaniBalwani <shivanibalwani3@gmail.com>
2018-10-05 13:42:21 +0530

    admin

--- main/app/Http/Controllers/Usercontroller.php
+++ main/app/Http/Controllers/Usercontroller.php
@@ -1,20 +1,18 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\User;
 
 class Usercontroller extends Controller
 {
     
-    public function showuser()
-    {
+    public function showuser(){
+
     	return view('admin.user.index')->with('users',User::all());
     }
      public function manager($id)
     {
-         $user=User::find($id);
+        $user=User::find($id);
         $user->manager=1;
         $user->visitor=0;
         $user->save();
--- main/resources/views/admin/user/index.blade.php
+++ main/resources/views/admin/user/index.blade.php
@@ -1,120 +1,53 @@
 @extends('layouts.adminlayout')
 @section('content')
 <section align="center" style="padding-top: 3%;" class="dashboard-counts">
+        @foreach($users as $user)
         <div class="container-fluid" >
           <div class="row">
             <!-- Count item widget-->
             <div  class="col-xl-12 col-sm-6">
               <div>
+                @if($user->permission == "administrator")
                 <img style="width: 10%; height: 10%;" src="{{asset('img/avatar-1.jpg')}}">  
-                <p>Administrator:-Dhruv Patel</p>  
+                
+                <p>{{$user->name}}</p>  
+                @endif
               </div>
             </div>
           </div>
         </div>
+        @endforeach
       </section>
-      @if($users->count()>0)
-			@foreach($users as $user)
-      <section class="dashboard-counts section-padding" align="center">
+      @foreach($users as $user)
+      @if($user->permission == "manager")
+			
+        <section class="dashboard-counts section-padding" align="center">
         <h2 style="text-align: center; margin-bottom: 3%;">Manager</h2>
         <div class="row">
-      <div class="col-lg-3 col-sm-6">
-        <div >
-          <img style="width: 80%; height: 80%;" src="{{asset('img/avatar-5.jpg')}}">
+        <div class="col-lg-3 col-sm-6">
+          <div >
+           <img style="width: 80%; height: 80%;" src="{{asset('img/avatar-5.jpg')}}">
+          </div>
+          <br>
         </div>
-        <br>
-        <div>
-        @if($user->manager)
-							<a href="{{route('user.visitor',['id'=>$user->id])}}"class="btn btn-primary">
-							Visitor</a>
-						@elseif($user->visitor)
-							<a href="{{route('user.manager',['id'=>$user->id])}}"class="btn btn-primary">
-							Manager</a>
-						@endif
         </div>
-      </div>
-      <div class="col-lg-3 col-sm-6">
-        <div>
-          <img style="width: 80%; height: 80%;" src="{{asset('img/avatar-4.jpg')}}">
-        </div>
-        <br>
-        <div>
-        @if($user->manager)
-              <a href="{{route('user.visitor',['id'=>$user->id])}}"class="btn btn-primary">
-              Visitor</a>
-            @elseif($user->visitor)
-              <a href="{{route('user.manager',['id'=>$user->id])}}"class="btn btn-primary">
-              Manager</a>
-            @endif
-        </div>
-           
-        </div>
-      <div class="col-lg-3 col-sm-6">
-        <div>
-          <img style="width: 80%; height: 80%;" src="{{asset('img/avatar-3.jpg')}}">
-        </div>
-        <br>
-        <div>
-        @if($user->manager)
-              <a href="{{route('user.visitor',['id'=>$user->id])}}"class="btn btn-primary">
-              Visitor</a>
-            @elseif($user->visitor)
-              <a href="{{route('user.manager',['id'=>$user->id])}}"class="btn btn-primary">
-              Manager</a>
-            @endif
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6">
-        <div>
-          <img style="width: 80%; height: 80%;" src="{{asset('img/avatar-2.jpg')}}">
-        </div>
-        <br>
-        <div>
-        @if($user->manager)
-              <a href="{{route('user.visitor',['id'=>$user->id])}}"class="btn btn-primary">
-              Visitor</a>
-            @elseif($user->visitor)
-              <a href="{{route('user.manager',['id'=>$user->id])}}"class="btn btn-primary">
-              Manager</a>
-            @endif
-        
-      </div>
-    </div>
       </section>
-      {{-- <section align="center" class="dashboard-counts section-padding">
-        <h2 style="text-align: center; margin-bottom: 3%;">Visitor</h2>
+      @endif
+
+      @if($user->permission == "visitor")
+      
+        <section class="dashboard-counts section-padding" align="center">
+        <h2 style="text-align: center; margin-bottom: 3%;">visitor</h2>
         <div class="row">
-      <div class="col-lg-3 col-sm-6">
-        <div>
-          <img style="width: 80%; height: 80%;" src="{{asset('img/avatar-6.jpg')}}">
+        <div class="col-lg-3 col-sm-6">
+          <div >
+           <img style="width: 80%; height: 80%;" src="{{asset('img/avatar-5.jpg')}}">
+            <p>{{$user->name}}</p>
+          </div>
+          <br>
         </div>
-        <p>Visitor</p>
-        <button>ADD</button>
-      </div>
-      <div class="col-lg-3 col-sm-6">
-        <div>
-          <img style="width: 80%; height: 80%;" src="{{asset('img/avatar-7.jpg')}}">
         </div>
-        <p>Visitor</p>
-        <button>ADD</button>
-      </div>
-      <div class="col-lg-3 col-sm-6">
-        <div>
-          <img style="width: 80%; height: 80%;" src="{{asset('img/avatar-8.jpg')}}">
-        </div>
-        <p>Visitor</p>
-        <button>ADD</button>
-      </div>
-      <div class="col-lg-3 col-sm-6">
-        <div>
-          <img style="width: 80%; height: 80%;" src="{{asset('img/avatar-9.jpg')}}">
-        </div>
-        <p>Visitor</p>
-        <button>ADD</button>
-      </div>
-    </div>
-    
-      </section> --}}
+      </section>
+      @endif
       @endforeach
-    @endif
 @stop
